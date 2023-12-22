cff11a030f9bc20238f632ba61244411ff874642
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2023-12-22 18:08:28 +0100

    prompt, random number

---
+++ array-tail/js/script.js
@@ -0,0 +1,36 @@
+/*
+2. La coda dell'Array
+
+Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali 
+(compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
+Stampiamo poi gli ultimi 5 elementi dell'Array
+
+Bonus:
+Chiediamo all'utente quanti elementi dell'array dovremo stampare
+Suggerimenti:
+Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi 
+totali dell'array
+*/
+
+//crea array: chiedi all'utente quanti elementi vuole (almeno 6)
+//generare numeri casuali 1-100
+//stampa solo gli ultimi 5
+
+const numberElements = prompt('Quanti elementi vuoi?')
+console.log(numberElements)
+
+const randomNumber = Math.floor(Math.random () * (100-5)) +1;
+
+const elements = (randomNumber);
+console.log(elements); 
+
+for (i=0; i<numberElements ;i++){
+    numberElements
+}
+
+
+
+   
+
+
+    
